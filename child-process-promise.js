9c8bbd55bdb965fad6786c148c84ab521936dba3
linusdamyo <linusdamyo@gmail.com>
2018-06-18 13:37:16 +0900

    get stderr text

--- child-process-promise.js
+++ child-process-promise.js
@@ -16,18 +16,21 @@ var childProcess = require('child_process'),
     'use strict';
     return new Promise(function (resolve, reject) {
       var process = childProcess.spawn(command, options);
+      var errText = '';
       process.stdout.on('data', function (buffer) {
-        console.log(buffer.toString());
+        //console.log(buffer.toString());
       });
       process.stderr.on('data', function (buffer) {
-        console.error(buffer.toString());
+        //console.error(buffer.toString());
+        errText = buffer.toString();
       });
       process.on('close', function (code) {
-        if (code !== 0) {
-          reject(code);
-        } else {
-          resolve();
-        }
+        resolve(errText);
+        //if (code !== 0) {
+        //  reject(code);
+        //} else {
+        //  resolve();
+        //}
       });
     });
   };
